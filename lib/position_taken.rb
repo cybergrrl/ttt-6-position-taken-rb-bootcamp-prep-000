7ef444cfd3a408aa67ed7703f31c7ede0eb895db
scout b <scout.burghardt@gmail.com>
2018-07-17 18:22:44 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(array, position)
-  if (array[position] == "X" || array[position] == "O")
+  if (array[position] == "X" || "O")
     return true
   else
     return false
